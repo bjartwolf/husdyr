61cef4c6844ea6e4858a02e8b02a0db80e60b772
bjartwolf <bjartnes@gmail.com>
2013-08-04 23:08:42 +0200

    Fikk kjeft av Jonas

--- src/husdyr.cljs
+++ src/husdyr.cljs
@@ -8,41 +8,37 @@
         (= (nth arr 1) val) 1
         (= (nth arr 2) val) 2
         (= (nth arr 3) val) 3
-        (= (nth arr 4) val) 4
-))
+        (= (nth arr 4) val) 4))
 ; Taken all ideas from here...
 ; http://programming-puzzler.blogspot.no/2013/03/logic-programming-is-overrated.html
 (defn solve-logic-puzzle [people]
-;    (first
-      (for [house (comb/permutations people)
-            :let [[nr1 nr2 nr3 nr4 nr5] house]  ; 1 
-            :when (= (people 1) nr1)
-            [yellow blue red green ivory] (comb/permutations people)  ; 1 
-            :when (= (- (indexOf house green) (indexOf house ivory)) 1) ;6
-            :when (= red (people 4)) ;2
-            [milk tea water orangejuice coffee] (comb/permutations people) 
-            :when (= (people 0) tea);5
-            :when (= green coffee) ;4
-            :when (= (nth house 2) milk)
-            [kools chesterfield oldgold parliament luckystrike] (comb/permutations people) 
-            :when (= yellow kools) ; 8
-            :when (= luckystrike orangejuice) ;13
-            [zebra snails dog horse fox] (comb/permutations people) 
-            :when (or (= (- (indexOf house chesterfield) (indexOf house fox)) 1)
-                      (= (+ (indexOf house chesterfield) (indexOf house fox)) 1))
-            :when (or (= (- (indexOf house kools) (indexOf house horse)) 1)
-                      (= (+ (indexOf house kools) (indexOf house horse)) 1))
-            :when (or (= (- (indexOf house (people 1)) (indexOf house blue)) 1) ;15
-                      (= (+ (indexOf house (people 1)) (indexOf house blue)) 1))
-            :when (= oldgold snails) ;7
-            :when (= (people 3) dog);3
-            :when (= (people 2) parliament)]; 13
+    (for [house (comb/permutations people)
+          :let [[nr1 nr2 nr3 nr4 nr5] house]  ; 1 
+          :when (= (people 1) nr1)
+          [yellow blue red green ivory] (comb/permutations people)  ; 1 
+          :when (= (- (indexOf house green) (indexOf house ivory)) 1) ;6
+          :when (= red (people 4)) ;2
+          [milk tea water orangejuice coffee] (comb/permutations people) 
+          :when (= (people 0) tea);5
+          :when (= green coffee) ;4
+          :when (= (nth house 2) milk)
+          [kools chesterfield oldgold parliament luckystrike] (comb/permutations people) 
+          :when (= yellow kools) ; 8
+          :when (= luckystrike orangejuice) ;13
+          [zebra snails dog horse fox] (comb/permutations people) 
+          :when (or (= (- (indexOf house chesterfield) (indexOf house fox)) 1)
+                    (= (+ (indexOf house chesterfield) (indexOf house fox)) 1))
+          :when (or (= (- (indexOf house kools) (indexOf house horse)) 1)
+                    (= (+ (indexOf house kools) (indexOf house horse)) 1))
+          :when (or (= (- (indexOf house (people 1)) (indexOf house blue)) 1) ;15
+                    (= (+ (indexOf house (people 1)) (indexOf house blue)) 1))
+          :when (= oldgold snails) ;7
+          :when (= (people 3) dog);3
+          :when (= (people 2) parliament)]; 13
 ; RETURN THE ANSWER        
-       (array-map
-          :yellow yellow :water water :kools kools :fox fox 
-          :zebra zebra :snails snails :dog dog :horse horse :fox fox :tea tea
-          )))
-;)
+     (array-map
+        :yellow yellow :water water :kools kools :fox fox 
+        :zebra zebra :snails snails :dog dog :horse horse :fox fox :tea tea)))
 
 (defn -main [& args]
        (def husdyr-config (nodejs/require "./husdyr.json"))
